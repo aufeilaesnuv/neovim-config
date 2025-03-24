5bdc8a5f3e8e2aa625e482022b5e0a2c0aa2b0a9
Jacob Youssef <jacob.youssef@gmail.com>
2025-03-24 15:39:45 -0400

    added keymap to get current file path

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -76,3 +76,10 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous dia
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- copy relative file path in clipboard
+vim.keymap.set("n", "<leader>fp", function()
+	local path = vim.fn.expand("%")
+	vim.fn.setreg("+", path)
+	print("Copied path: " .. path)
+end, opts)
