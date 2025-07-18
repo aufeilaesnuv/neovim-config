ebcf0dc025439765155c8045aaacd564d7be683c
Jacob Youssef <jacob.youssef@gmail.com>
2025-07-18 13:50:25 -0400

    removed unused misc plugins

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,14 +1,6 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
 	{
-		-- Tmux & split window navigation
-		-- "christoomey/vim-tmux-navigator",
-	},
-	{
-		-- Detect tabstop and shiftwidth automatically
-		-- 'tpope/vim-sleuth',
-	},
-	{
 		-- Powerful Git integration for Vim
 		"tpope/vim-fugitive",
 	},
